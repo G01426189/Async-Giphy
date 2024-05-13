9a8ba1400d067f62acd5ff653200bd8d8b5f7172
G01426189 <165887924+G01426189@users.noreply.github.com>
2024-05-13 16:25:52 +0100

    Updated

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 require('dotenv').config();
 
 // Print out value of API key stored in .env file
+
+let endpoint = 'https://api.giphy.com/v2/emoji?api_key=07vNm1dGuINPnU0Dem1ZmoT1RRQ4wVdn&limit=25&offset=0'
+async function getImage(query){
+    try {
+        const response = await fetch(endpoint + "&q=" + query);
+        const data = await response.json();
+        const imageURL = data.data[0].url;
+        console.log(imageURL, "imageURL");
+        return imageURL;
+    } catch (error){
+        console.error ("This image can't be fetched", error);
+    }
+
+}
 console.log(process.env.API_KEY)
